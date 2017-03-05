b84e26b834fd0c532b057e104f0095ff0e5c9c67
nelson1987 <nelson.ash@outlook.com>
2017-03-05 13:07:26 -0300

    Criacao da Arquitetura
    Divisão de Pastas e arquivo base de todas as camadas

---
+++ 5_CrossCutting/Ephimes.CrossCutting/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace Ephimes.Domain.Entities
+{
+    public class Usuario
+    {
+        protected Usuario()
+        {
+        }
+
+        public Usuario(string login)
+        {
+            Login = login;
+        }
+
+        public string Login { get; private set; }
+    }
+}
