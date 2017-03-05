15a7f3c380123dc8e5b5c7b7ad6e25684a124347
nelson1987 <nelson.ash@outlook.com>
2017-03-05 13:17:49 -0300

    Dados No Assembly
    Assembly

--- 5_CrossCutting/Ephimes.CrossCutting/Properties/AssemblyInfo.cs
+++ 5_CrossCutting/Ephimes.CrossCutting/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Ephimes.Domain")]
+[assembly: AssemblyTitle("Ephimes.CrossCutting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ephimes.Domain")]
+[assembly: AssemblyProduct("Ephimes.CrossCutting")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
