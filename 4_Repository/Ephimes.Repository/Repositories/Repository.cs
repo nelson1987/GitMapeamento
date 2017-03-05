fa236dab4bcba16ce547ab9597859c2a5425b1fa
nelson1987 <nelson.ash@outlook.com>
2017-03-05 14:21:14 -0300

    Packages
    Descarte de Pacotes

--- 4_Repository/Ephimes.Repository/Repositories/Repository.cs
+++ 4_Repository/Ephimes.Repository/Repositories/Repository.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
 using Ephimes.Domain.Interfaces.Repositories;
 
 namespace Ephimes.Repository.Repositories
